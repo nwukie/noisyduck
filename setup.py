cab466cb3ac0f3c0bb5e5b09167bb27ed6613b73
Nathan A. Wukie <nwukie@gmail.com>
2018-02-04 23:04:46 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ test_requirements = [
 
 setup(
     name='noisyduck',
-    version='0.1.0',
+    version='0.1.1',
     description="A python tool for computing eigenmode decompositions of duct flows.",
     long_description=readme + '\n\n' + history,
     author="Nathan Wukie",
